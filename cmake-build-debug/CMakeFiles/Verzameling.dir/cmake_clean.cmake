c37c4f3ef63c2abd7b9df68e75abff2295e8a8a2
Tibo Rigole <tibo.rigole@gmail.com>
2018-07-30 15:28:36 +0200

    geen errors, nog opschonen

---
+++ cmake-build-debug/CMakeFiles/Verzameling.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Verzameling.dir/main.cpp.obj"
+  "Verzameling.pdb"
+  "Verzameling.exe"
+  "Verzameling.exe.manifest"
+  "libVerzameling.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Verzameling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,219 @@
+#include <iostream>
+using namespace std;
+
+class Verzameling{
+private:
+    int grootte;
+    int *rij;
+    int aantalIngevuld;
+
+public:
+
+    //gewone constructor
+    Verzameling(){
+        grootte = 10;
+        aantalIngevuld = 0;
+        rij = new int[10];
+    }
+
+    //constructor met grootte parameter
+    Verzameling(int gr){
+        grootte = gr;
+        aantalIngevuld = 0;
+        rij = new int[gr];
+    }
+
+    bool bevatGetal(int getal){
+        //returnt 0 als het getal niet in de array zit
+        //returnt 1 als het getal in de rij zit
+        bool aanwezig = false;
+
+        for(int i=0 ; i<aantalIngevuld; i++){
+            if (rij[i] == getal){aanwezig = true;}
+        }
+
+        return aanwezig;
+    }
+
+    int pakGetalOpPlek(int i){
+        return rij[i];
+    }
+
+    //destructor
+    ~Verzameling(){
+        cout <<"destructor opgeroepen"<<endl;
+        delete rij;
+    }
+
+    void add(int getal){
+        //als aantalIngevuld < grootte-1 , dan is de rij al ingevuld, moeten we het 1tje vergroten
+        if(aantalIngevuld >= grootte-1){
+            //nog totaal niet zeker als dit juist is, zowieso nog aangepast
+            cout<<"de rij is te klein, het elem past er niet meer in"<<endl;
+            delete rij;
+            //doe nog zaken, zoals maak nieuwe rij aan, kopieer alle elementen,...
+        }
+        else{
+            //cout << "het element past der wel nog in"<<endl;
+            //dus als de rij niet te klein is
+            //misschien moet der hier nog sterretje ofzo
+            rij[aantalIngevuld] = getal;
+            aantalIngevuld ++;
+        }
+    }
+
+
+
+    //output operator
+    friend ostream& operator<< (ostream& os, Verzameling v);    //buiten de klasse verder afgewerkt
+
+    //operator* overloading
+    friend Verzameling operator*(Verzameling a, Verzameling b);
+
+    //operator+ overloading
+    friend Verzameling operator+(Verzameling a, Verzameling b);
+
+    //operator+ overloading aan de hand van =/= paramters
+    friend Verzameling operator+(Verzameling a, int getal);
+
+    friend Verzameling operator+(int getal, Verzameling a);
+};
+
+ostream& operator<< (ostream& os, Verzameling v){
+    cout <<"{";
+    for(int i=0; i<v.aantalIngevuld ; i++){
+        cout<< v.rij[i];
+        if(i != v.aantalIngevuld-1){cout <<",";}
+    }
+    cout<<"}"<<endl;
+
+    //return dit allemaal?
+}
+
+Verzameling operator*(Verzameling a, Verzameling b){
+    Verzameling vres;
+
+    for(int i=0; i<a.aantalIngevuld; i++){
+        int getal = a.pakGetalOpPlek(i);
+
+        if(b.bevatGetal(getal)){vres.add(getal);}
+    }
+
+    return vres;
+
+}
+
+Verzameling operator+(Verzameling a, Verzameling b){
+
+    int grootte = a.grootte+b.grootte;
+
+    Verzameling vres(grootte);
+
+    for(int i=0; i<a.aantalIngevuld; i++){
+        int getal = a.pakGetalOpPlek(i);
+
+        vres.add(getal);
+    }
+
+    for(int j=0; j<b.aantalIngevuld; j++){
+        int getal = b.pakGetalOpPlek(j);
+
+        if(!a.bevatGetal(getal)){vres.add(getal);}
+    }
+
+    return vres;
+}
+
+Verzameling operator+(Verzameling a, int getal){
+    cout <<"de operator+  met op 2e plek een int"<<endl;
+
+    int grootte = a.grootte+1;
+    Verzameling vres(grootte);
+
+    for(int i=0; i<a.aantalIngevuld; i++){
+        int rijgetal = a.pakGetalOpPlek(i);
+
+        vres.add(rijgetal);
+    }
+
+    vres.add(getal);
+
+    return vres;
+}
+
+Verzameling operator+(int getal, Verzameling a){
+    cout <<"de operator+  met op 1e plek een int"<<endl;
+
+    int grootte = a.grootte+1;
+    Verzameling vres(grootte);
+
+    vres.add(getal);
+
+    for(int i=0 ; i<a.aantalIngevuld; i++){
+        int rijgetal = a.pakGetalOpPlek(i);
+
+        vres.add(rijgetal);
+    }
+
+    return vres;
+}
+
+
+
+
+
+int main()
+{
+
+    Verzameling v1; //default 10 plaatsen
+    Verzameling v2(20); //parameter -> aantal plaatsen
+
+    for (int i=1;i<10;i++) v1.add(i);
+    cout <<"v1: "<<endl;
+
+    cout <<v1<<endl; //moet nog weg, was om te checken alst paste
+
+    for (int i=1;i<10;i++) v2.add(2*i);
+    cout <<"v2: "<<endl;
+    cout <<v2<<endl; //moet nog weg, was om te chekcen alst paste
+
+    Verzameling v3 = v2;
+    cout <<"v3"<<endl;
+    cout <<v3<<endl;
+
+    Verzameling v4,v5;
+
+    bool test = v3.bevatGetal(17);
+    cout<<"test: "<<test<<endl;
+
+    test = v3.bevatGetal(4);
+    cout<<"test: "<<test<<endl;
+
+    int getal1 = v3.pakGetalOpPlek(3);
+    cout<<getal1<<endl;
+
+    v4 = v1 + v2; //unie
+    cout<<"v4"<<endl;
+    cout<<v4<<endl;
+
+    //die operator = heeft misschien nog nodig, want deze kan fout oproepen
+    v5 = v1 * v2; //doorsnede
+
+    cout<<"v5: "<<endl;
+    cout <<v5<<endl;
+            /*
+    cout << v4 << endl; //getallen gescheiden door ,
+                        //en omringd door {}
+    cout << v5 << endl;
+            */
+    v4 = v4 + 84; //casting
+
+
+    v5 = 20 + v1;
+    cout <<"v4+84"<<endl;
+    cout << v4 << endl;
+    cout <<"20+v5"<<endl;
+    cout << v5 << endl;
+
+    return 0;
+}
